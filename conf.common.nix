517faa5eb2d477105aa2f631f08e17a1fe1e6d0c
Jack J. Mao <junchih.1893@gmail.com>
2022-12-23 19:07:49 +0800

    Update lbnuc

--- conf.common.nix
+++ conf.common.nix
@@ -1,11 +1,13 @@
 { ... }:
 
 {
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "22.05"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
 
   # Set your time zone.
   time.timeZone = "Asia/Hong_Kong";
